55916933f8e6be6abb548dae7e3f9e0e1c65b961
Soul Pro <70108490+ESinclair81@users.noreply.github.com>
2020-11-12 14:13:25 -0500

    Added generate webpage function

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ const profileDataArgs = process.argv.slice(2, process.argv.length);
 // const github = profileDataArgs[1];
 const [name, github] = profileDataArgs;
 
-
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
 
 
 
