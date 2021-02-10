037a406eeb7c3e675ef5c451b9400c97d700d5db
Soul Pro <70108490+ESinclair81@users.noreply.github.com>
2021-02-10 11:52:53 -0500

    updated page-temnplate HTML and restored write to index.html

--- app.js
+++ app.js
@@ -2,14 +2,6 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generatePage = require('./src/page-template');
 
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-// console.log('Portfolio complete! Check out index.html to see the output!');
-// });
-
 
 //*______________ INQUIRER PROMPTS ______________________________*//
 const promptUser = () => {
@@ -55,17 +47,18 @@ const promptUser = () => {
 //*_________________________________ Project Questions Function _____________________________________*//
 const promptProject = portfolioData => {
 
-  // If there's no 'projects' array property, create one
-if (!portfolioData.projects) {
-  portfolioData.projects = [];
-}
-
   console.log(`
 =================
 Add a New Project
 =================
 `);
 
+
+  // If there's no 'projects' array property, create one
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
+  
   return inquirer.prompt([
     
     {
@@ -159,8 +152,13 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
-    // const pageHTML = generatePage(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+  fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw err;
+  
+  console.log('Portfolio complete! Check out index.html to see the output!');
+  });
 
   });
 
